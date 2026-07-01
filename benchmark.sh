26ed10b4f5576278db0c5b6dae5be8af875fcb14
Christoffel Lombard <stoflom@gmail.com>
2026-07-01 11:41:35 +0200

    Changed default benchmark to Qwen3.6

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,3 @@
-CMD=(llama.cpp/build/bin/llama-bench -hf unsloth/Qwen3.5-35B-A3B-GGUF:UD-Q4_K_XL -p 512 -n 256 "$@")
+CMD=(llama.cpp/build/bin/llama-bench -hf unsloth/Qwen3.6-35B-A3B-MTP-GGUF:UD-Q4_K_M -p 512 -n 256 "$@")
 echo "CMD: ${CMD[*]}"
 exec "${CMD[@]}"
